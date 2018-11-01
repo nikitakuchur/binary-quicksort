25a319bca56fd1590131ae6df8e71ece345cbdc5
Nikita Kuchur <nikitakuchur@gmail.com>
2018-11-02 00:04:47 +0300

    Added sort animation

--- src/sort/Controller.java
+++ src/sort/Controller.java
@@ -3,19 +3,19 @@ package sort;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 
 import java.net.URL;
-import java.util.Random;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
     public TextField size;
+
+    @FXML
+    public Button generateButton;
 
     @FXML
     public Pane pane;
@@ -26,71 +26,59 @@ public class Controller implements Initializable {
     @FXML
     public Button playButton;
 
-    private Double[] arr;
+    @FXML
+    public Button stopButton;
+
+    private BinaryQuicksortPanel bqPanel;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        arr = generateArray(100);
-        draw(canvas.getGraphicsContext2D());
+        bqPanel = new BinaryQuicksortPanel(canvas);
+
+        bqPanel.draw();
 
         pane.prefWidthProperty().bind(pane.widthProperty());
         pane.prefHeightProperty().bind(pane.heightProperty());
 
         pane.prefWidthProperty().addListener((ov, oldValue, newValue) -> {
             canvas.setWidth(newValue.doubleValue());
-            draw(canvas.getGraphicsContext2D());
+            bqPanel.draw();
         });
 
         pane.prefHeightProperty().addListener((ov, oldValue, newValue) -> {
             canvas.setHeight(newValue.doubleValue());
-            draw(canvas.getGraphicsContext2D());
+            bqPanel.draw();
+        });
+
+        bqPanel.setOnRunning(t -> {
+            generateButton.setDisable(true);
+            playButton.setDisable(true);
+            stopButton.setDisable(false);
+        });
+
+        bqPanel.setOnSucceeded(t -> {
+            bqPanel.reset();
+        });
+
+        bqPanel.setOnReady(t -> {
+            generateButton.setDisable(false);
+            playButton.setDisable(false);
+            stopButton.setDisable(true);
         });
     }
 
     public void handleRunButtonClick() {
-        BinaryQuicksort.sort(arr, Double.SIZE, (number, i) -> {
-            long bNumber = Double.doubleToLongBits(number);
+        bqPanel.start();
+    }
 
-            // Check sign
-            if (number < 0) {
-                if (i == 0)
-                    return 0;
-                bNumber = ~bNumber;
-            }
-            else if (i == 0)
-                return 1;
-
-            return bNumber >> (Double.SIZE - i - 1) & 1;
-        });
-
-        draw(canvas.getGraphicsContext2D());
+    public void handleStopButtonClick() {
+        bqPanel.cancel();
+        bqPanel.reset();
     }
 
     public void handleGenerateButtonClick() {
         int size = Integer.parseInt(this.size.getText());
-        arr = generateArray(size);
-        draw(canvas.getGraphicsContext2D());
+        bqPanel.generateArray(size);
+        bqPanel.draw();
     }
-
-    private void draw(GraphicsContext gc) {
-        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-
-        double w = canvas.getWidth() / arr.length;
-        for (int i = 0; i < arr.length; i++) {
-            double h = canvas.getHeight() * arr[i];
-            gc.setFill(Color.hsb(arr[i] * 60, 0.9, 0.9));
-            gc.fillRect(i * w, canvas.getHeight() - h, w + 1, h);
-        }
-    }
-
-    private Double[] generateArray(int size) {
-        Random rand = new Random();
-        Double[] arr = new Double[size];
-
-        for (int i = 0; i < size; i++)
-            arr[i] = rand.nextDouble();
-
-        return arr;
-    }
-
 }
