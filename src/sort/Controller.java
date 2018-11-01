42c0c253ad78cadc1af71503ed351860c195aa3e
Nikita Kuchur <nikitakuchur@gmail.com>
2018-11-01 16:35:07 +0300

    Added array and sorting

--- src/sort/Controller.java
+++ src/sort/Controller.java
@@ -5,13 +5,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    @FXML
+    public TextField size;
+
     @FXML
     public Pane pane;
 
@@ -21,8 +26,11 @@ public class Controller implements Initializable {
     @FXML
     public Button playButton;
 
+    private Double[] arr;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        arr = generateArray(100);
         draw(canvas.getGraphicsContext2D());
 
         pane.prefWidthProperty().bind(pane.widthProperty());
@@ -40,11 +48,49 @@ public class Controller implements Initializable {
     }
 
     public void handleRunButtonClick() {
+        BinaryQuicksort.sort(arr, Double.SIZE, (number, i) -> {
+            long bNumber = Double.doubleToLongBits(number);
 
+            // Check sign
+            if (number < 0) {
+                if (i == 0)
+                    return 0;
+                bNumber = ~bNumber;
+            }
+            else if (i == 0)
+                return 1;
+
+            return bNumber >> (Double.SIZE - i - 1) & 1;
+        });
+
+        draw(canvas.getGraphicsContext2D());
+    }
+
+    public void handleGenerateButtonClick() {
+        int size = Integer.parseInt(this.size.getText());
+        arr = generateArray(size);
+        draw(canvas.getGraphicsContext2D());
     }
 
     private void draw(GraphicsContext gc) {
-        gc.setFill(Color.WHITE);
-        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        double w = canvas.getWidth() / arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            double h = canvas.getHeight() * arr[i];
+            gc.setFill(Color.hsb(arr[i] * 60, 0.9, 0.9));
+            gc.fillRect(i * w, canvas.getHeight() - h, w + 1, h);
+        }
     }
+
+    private Double[] generateArray(int size) {
+        Random rand = new Random();
+        Double[] arr = new Double[size];
+
+        for (int i = 0; i < size; i++)
+            arr[i] = rand.nextDouble();
+
+        return arr;
+    }
+
 }
--- src/sort/Digitizer.java
+++ src/sort/Digitizer.java
@@ -1,5 +1,5 @@
 package sort;
 
 public interface Digitizer<T> {
-    int getDigit(T object, int i);
+    long getDigit(T object, int i);
 }
