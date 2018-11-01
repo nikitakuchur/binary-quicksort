88e7a2cfcca0d27ad0645c05ab2479a48ef8a3af
Nikita Kuchur <nikitakuchur@gmail.com>
2018-11-01 14:33:42 +0300

    Changed the window size

--- src/sort/Main.java
+++ src/sort/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
         primaryStage.setTitle("Binary Quicksort");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
