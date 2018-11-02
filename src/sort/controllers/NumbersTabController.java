82ace516aabb7df11c1e3e2edcd9a9c6dc9c303f
Nikita Kuchur <nikitakuchur@gmail.com>
2018-11-02 17:54:43 +0300

    Added sorting for numbers

--- src/sort/controllers/NumbersTabController.java
+++ src/sort/controllers/NumbersTabController.java
@@ -1,4 +1,87 @@
 package sort.controllers;
 
-public class NumbersTabController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.input.KeyCode;
+import sort.BinaryQuicksort;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NumbersTabController implements Initializable {
+
+    @FXML
+    public ListView<String> list;
+
+    @FXML
+    public ListView<String> sortedList;
+
+    public ObservableList<String> items;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        items = FXCollections.observableArrayList();
+        list.setCellFactory(TextFieldListCell.forListView());
+        list.setItems(items);
+        list.getItems().add("");
+
+        list.setOnEditCommit(t -> {
+            try {
+                String str = t.getNewValue();
+                int num = Integer.parseInt(str);
+                list.getItems().set(t.getIndex(), "" + num);
+                if (t.getIndex() == list.getItems().size() - 1)
+                    list.getItems().add("");
+            } catch (NumberFormatException ex) {
+            }
+        });
+
+        list.setOnKeyPressed(e -> {
+            String item = list.getSelectionModel().getSelectedItem();
+
+            if (item == null || item.isEmpty())
+                return;
+
+            if (e.getCode().equals(KeyCode.DELETE))
+                list.getItems().remove(item);
+
+            if (list.getItems().isEmpty())
+                list.getItems().add("");
+        });
+
+        sortedList.getItems().add("");
+    }
+
+    @FXML
+    public void handleSortButtonClick() {
+        int size = items.size() - 1;
+        Integer[] array = new Integer[size];
+
+        for (int i = 0; i < size; i++)
+            array[i] = Integer.parseInt(items.get(i));
+
+        // Sort
+        BinaryQuicksort.sort(array, Integer.SIZE, (number, i) -> {
+
+            // Check sign
+            if (number < 0 && i == 0)
+                return 0;
+            else if(i == 0)
+                return 1;
+
+            return (number >> (Integer.SIZE - i - 1)) & 1;
+        });
+
+        sortedList.getItems().clear();
+
+        // Add to the sortedList
+        for (int i = 0; i < size; i++)
+            sortedList.getItems().add("" + array[i]);
+
+        sortedList.getItems().add("");
+    }
 }
